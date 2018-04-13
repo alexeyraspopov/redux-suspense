bd0042e6d96a0d4ffe917415f1a07d06dcc35cbe
Alexey Raspopov <oleksii.raspopov@gmail.com>
2018-04-13 16:13:42 -0400

    babelify build artifact

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,13 @@
+import babel from 'rollup-plugin-babel';
 import entries from 'rollup-plugin-multi-entry';
+
+let babelConfig = {
+  babelrc: false,
+  presets: [
+    ['env', { modules: false }],
+  ],
+  plugins: ['external-helpers'],
+};
 
 export default {
   input: 'modules/*.js',
@@ -6,6 +15,6 @@ export default {
     file: 'build/redux-suspense.js',
     format: 'cjs',
   },
-  plugins: [entries()],
+  plugins: [entries(), babel(babelConfig)],
   external: ['react', 'react-redux'],
 };
