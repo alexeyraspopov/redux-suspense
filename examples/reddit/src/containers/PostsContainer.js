cfabd8663b590fc85cf00996132bc0e207aad45c
Alexey Raspopov <oleksii.raspopov@gmail.com>
2018-04-24 00:55:04 -0400

    render props > hocs

--- examples/reddit/src/containers/PostsContainer.js
+++ examples/reddit/src/containers/PostsContainer.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import { connectSuspense } from 'redux-suspense';
+import { Consumer } from 'redux-suspense';
 import postsMapper from '../mappers/postsMapper';
 import Posts from '../components/Posts';
 
-export default connectSuspense(PostsContainer);
-
-function PostsContainer({ state, dispatch, subreddit }) {
-  let { items: posts } = postsMapper.read(state, dispatch, subreddit);
+export default function PostsContainer({ subreddit }) {
   return (
-    <section>
-      <Posts posts={posts} />
-    </section>
+    <Consumer>
+      {({ state, dispatch }) => {
+        let { items: posts } = postsMapper.read(state, dispatch, subreddit);
+        return (
+          <section>
+            <Posts posts={posts} />
+          </section>
+        );
+      }}
+    </Consumer>
   );
 }
--- examples/reddit/src/containers/PostsOptimisticContainer.js
+++ examples/reddit/src/containers/PostsOptimisticContainer.js
@@ -1,18 +1,22 @@
 import React from 'react';
-import { connectSuspense } from 'redux-suspense';
+import { Consumer } from 'redux-suspense';
 import postsMapper from '../mappers/postsMapper';
 import Posts from '../components/Posts';
 
-export default connectSuspense(PostsOptimisticContainer);
-
-function PostsOptimisticContainer({ state, subreddit }) {
-  let posts = postsMapper.peek(state, subreddit);
-  if (!posts || posts.items.length === 0) {
-    return <p>Loading...</p>;
-  }
+export default function PostsOptimisticContainer({ subreddit }) {
   return (
-    <section style={{ opacity: 0.5 }}>
-      <Posts posts={posts.items} />
-    </section>
+    <Consumer>
+      {({ state }) => {
+        let posts = postsMapper.peek(state, subreddit);
+        if (!posts || posts.items.length === 0) {
+          return <p>Loading...</p>;
+        }
+        return (
+          <section style={{ opacity: 0.5 }}>
+            <Posts posts={posts.items} />
+          </section>
+        );
+      }}
+    </Consumer>
   );
 }
