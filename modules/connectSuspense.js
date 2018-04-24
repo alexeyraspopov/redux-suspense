a7d92c93d0bbe1023d2514992848fed371aedcfe
Alexey Raspopov <oleksii.raspopov@gmail.com>
2018-04-24 00:48:28 -0400

    represent connectSuspense using Consumer

--- modules/connectSuspense.js
+++ modules/connectSuspense.js
@@ -1,35 +1,12 @@
 import React from 'react';
-import { Placeholder, PlaceholderContext } from './Placeholder';
-import { connect } from 'react-redux';
+import { Consumer } from './Consumer';
 
 export function connectSuspense(coroutine) {
-  function SuspenseWrapper(props) {
+  return function SuspenseWrapper(props) {
     return (
-      <PlaceholderContext.Consumer>
-        {(trigger) => {
-          if (!trigger) {
-            return (
-              <Placeholder delayMs={0} fallback={null}>
-                <SuspenseWrapper {...props} />
-              </Placeholder>
-            );
-          }
-
-          try {
-            return coroutine(props);
-          } catch (effect) {
-            if (effect instanceof Promise) {
-              trigger(true);
-              effect.then(() => trigger(false), () => trigger(false));
-              return null;
-            }
-
-            throw effect;
-          }
-        }}
-      </PlaceholderContext.Consumer>
+      <Consumer>
+        {(store) => coroutine(Object.assign({}, store, props))}
+      </Consumer>
     );
   }
-
-  return connect(state => ({ state }))(SuspenseWrapper);
 }
